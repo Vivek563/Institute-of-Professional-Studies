8d0fe5dd9a41712da553d65f3894f332baf8295a
Prakhar-Verma39 <103757447+Prakhar-Verma39@users.noreply.github.com>
2023-04-04 04:56:34 +0530

    updating error page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 
@@ -9,7 +11,7 @@ const morgan = require('morgan');
 // app.use(morgan('tiny'));
 
 const pages = require('./pages.json'); 
-const ExpressError = require('./utils/ExpressError');
+const navbarItems = { ...pages["home"] }.navbarItems;
 
 // const Centres = require('./models/centres');
 
@@ -23,47 +25,33 @@ const ExpressError = require('./utils/ExpressError');
 // })
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.set('view engine', 'ejs'); // (property, value)
+app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, '/views'))
 
 // app.use(express.urlencoded({extended: true}))
 // app.use(methodOverride('_method'))
 
-function wrapAsync(fn){
-    return function(req, res, next){
-        fn(req, res, next).catch(e => next(e))
-    }
-}
 
-
-app.get('/', wrapAsync(async (req, res, next) => {
+app.get('/', catchAsync(async (req, res) => {
         const home = await pages["home"];
         res.render('home.ejs', { ...home })
     }))
 
-app.get('/:template', wrapAsync(async (req, res, next) => {
+app.get('/:template', catchAsync(async (req, res) => {
     const {template} = req.params;
     const home = await pages["home"];
     res.render(`template/${template}.ejs`, {...home})
 }))
 
-app.get('/home/:centre',  wrapAsync(async (req, res, next) =>{
+app.get('/home/:centre',  catchAsync(async (req, res) =>{
     const navbarItems = await { ...pages["home"] }.navbarItems;
     const {centre} = req.params;
     const page = await pages[centre];
-    if(!page){
-        const path = "../"
-        const style = "css/centre.css"
-        const script = "js/centre.js"
-        const title = "Error"
-    res.render('notfound.ejs', {path, style, title,  script, navbarItems, centre })}
-    else{
     res.render('centre.ejs', { ...page, navbarItems})} 
-}))
+))
 
 
-// app.get('/home/:centre/about', wrapAsync(async (req, res, next) => {
+// app.get('/home/:centre/about', catchAsync(async (req, res) => {
 //     const about = await pages['about'];
 //     const navbarItems = await { ...pages["home"] }.navbarItems;
 //     const {centre} = req.params;
@@ -72,7 +60,7 @@ app.get('/home/:centre',  wrapAsync(async (req, res, next) =>{
 //     res.render('template/about.ejs', {...page, navbarItems})
 // }))
 
-// app.get('/home/:centre/courses', wrapAsync(async (req, res, next) => {
+// app.get('/home/:centre/courses', catchAsync(async (req, res) => {
 //     const courses = await pages['courses'];
 //     const navbarItems = await { ...pages["home"] }.navbarItems;
 //     const {centre} = req.params;
@@ -81,7 +69,7 @@ app.get('/home/:centre',  wrapAsync(async (req, res, next) =>{
 //     res.render('template/courses.ejs', {...page, navbarItems})
 // }))
 
-// app.get('/home/:centre/faculty', wrapAsync(async (req, res, next) => {
+// app.get('/home/:centre/faculty', catchAsync(async (req, res) => {
 //     const faculty = await pages['faculty'];
 //     const navbarItems = await { ...pages["home"] }.navbarItems;
 //     const {centre} = req.params;
@@ -90,7 +78,7 @@ app.get('/home/:centre',  wrapAsync(async (req, res, next) =>{
 //     res.render('template/faculty.ejs', {...page, navbarItems})
 // }))
 
-// app.get('/home/:centre/gallery', wrapAsync(async (req, res, next) => {
+// app.get('/home/:centre/gallery', catchAsync(async (req, res) => {
 //     const gallery = await pages['gallery'];
 //     const navbarItems = await { ...pages["home"] }.navbarItems;
 //     const {centre} = req.params;
@@ -99,7 +87,7 @@ app.get('/home/:centre',  wrapAsync(async (req, res, next) =>{
 //     res.render('template/gallery.ejs', {...page, navbarItems})
 // }))
 
-// app.get('/home/:centre/notice', wrapAsync(async (req, res, next) => {
+// app.get('/home/:centre/notice', catchAsync(async (req, res) => {
 //     const notice = await pages['notice'];
 //     const navbarItems = await { ...pages["home"] }.navbarItems;
 //     const {centre} = req.params;
@@ -109,8 +97,8 @@ app.get('/home/:centre',  wrapAsync(async (req, res, next) =>{
 // }))
 
 
-app.get('*', (req, res, next) => {
-  next(new ExpressError('Requested Page Not Found', 404));  
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Requested Page Not Found', 404)) 
 })
 
 
@@ -118,8 +106,16 @@ app.use((err, req, res, next) => {
     console.log("************************************")
     console.log("**************ERROR*****************")
     console.log("************************************")
-    const {status = 500, message = 'Something went wromg'} = err;
-    res.status(status).send(message)
+    
+    const path = "../"
+    const style = "css/centre.css"
+    const script = "js/centre.js"
+    const title = "Error"
+
+
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('notfound', {path, style, title,  script, navbarItems, err })
 })
 
 
