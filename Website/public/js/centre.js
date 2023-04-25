cbd6bd7483a059b501afc4833e61a3d8d21c7c46
Vivek maurya <vivek563maurya@gmail.com>
2023-04-25 16:36:18 +0530

    "centre updated"

--- Website/public/js/centre.js
+++ Website/public/js/centre.js
@@ -1,15 +1,72 @@
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener('click', function (e) {
+e.preventDefault();
 
-/*---------------------------------centre's new section------------------------------------------*/
-
-const menuOpen = document.querySelector(".menu-open");
-const menuClose = document.querySelector(".menu-close");
-const sideHeader = document.querySelector(".sideHeader");
-
-menuOpen.addEventListener("click", () => {
-  sideHeader.classList.add("display");
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+});
+});
 });
 
-menuClose.addEventListener("click", () => {
-  sideHeader.classList.remove("display");
-});
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+
+// View more btn
+function showHide(btnID, textID){
+var showMoreText =
+            document.getElementById(textID);
+
+var buttonText =
+            document.getElementById(btnID);
+
+buttonText.onclick = function() {
+    if(buttonText.innerText === "View more"){
+        showMoreText.style.display = "inline";
+        buttonText.innerText = "View less";
+    }
+    else {
+        showMoreText.style.display = "none";
+        buttonText.innerText = "View more";
+    }
+}
+
+}
+
+// <--------------------------------- reusable  Dialog  --------------------------------------------->
+
+function dialogFiller(c){
+    let c_object = JSON.parse(c);
+    document.getElementById("reusableDialogContent").innerHTML = `<h2>${c_object.title}</h2><p>${c_object.description}</p><a href="${c_object.pdfUrl}" target="_blank">oo</a>`;
+  }
+  
+  document.querySelectorAll(".dialogTrigger").forEach( item => { 
+      item.addEventListener("click",  () => {
+        document.querySelector("#reusableDialog").showModal();
+  })
+  })
+  
